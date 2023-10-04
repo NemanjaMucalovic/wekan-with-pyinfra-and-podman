4f2635e257fdea6ed0d7c968198969441ed1854e
Nemanja Mucalovic <nemanjamucalovic@gmail.com>
2023-10-05 00:07:38 +0200

    templates dir created,kube is working,all tests are passing

--- deployment.py
+++ deployment.py
@@ -1,4 +1,10 @@
-from pyinfra.operations import dnf, server, files
+from pyinfra.operations import dnf, server, files, systemd
+
+
+def get_testinfra_username(host):
+    """Gets the username for Testinfra from group_data."""
+    return host.get_group_data("wekan", "ssh_user")
+
 
 dnf.packages(
     name="install podman",
@@ -7,26 +13,39 @@ dnf.packages(
     _sudo=True,
 )
 
-server.shell(name="pull container images", commands=["podman pull docker.io/library/mongo; podman pull docker.io/wekanteam/wekan"])
+server.shell(
+    name="pull container images",
+    commands=[
+        "podman pull docker.io/library/mongo; podman pull docker.io/wekanteam/wekan"
+    ],
+)
 
 files.directory(
     name="create project directory",
-    path="~/wekan/",
+    path="/home/nemanja/wekan/",
 )
 
 files.template(
-    name="Create kube file",
-    src="templates/kube.j2",
-    dest="~/wekan/kube.yml"
+    name="Create kube file", src="templates/kube.j2", dest="/home/nemanja/wekan/kube.yml"
 )
 
 files.directory(
     name="create dir for quadlet files",
-    path="~/.config/containers/systemd",
+    path="/home/nemanja/.config/containers/systemd",
 )
 
 files.template(
     name="Create podman service file",
     src="templates/wekan.j2",
-    dest="~/.config/containers/systemd/wekan.kube",
+    dest="/home/nemanja/.config/containers/systemd/wekan.kube",
 )
+
+systemd.service(
+    name="Start kube service",
+    service="wekan",
+    running=True,
+    daemon_reload=True,
+    user_mode=True,
+    user_name="nemanja"
+)
+
--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -8,33 +8,34 @@ def test_if_images_are_pulled(host):
 
 
 def test_if_wekan_dir_is_there(host):
-    assert host.file("~/wekan").is_directory
+    assert host.file("/home/nemanja/wekan").is_directory
 
 
 def test_if_kube_yaml_is_copied(host):
-    assert host.file("~/wekan/kube.yml").exists
+    assert host.file("/home/nemanja/wekan/kube.yml").exists
 
 
 def test_if_systemd_dir_is_there(host):
-    assert host.file("~/.config/containers/systemd/").is_directory
+    assert host.file("/home/nemanja/.config/containers/systemd/").is_directory
 
 
 def test_if_quadlet_yaml_is_copied(host):
-    assert host.file("~/.config/containers/systemd/wekan.kube").exists
+    assert host.file("/home/nemanja/.config/containers/systemd/wekan.kube").exists
 
 
 def test_backend_pod_is_running(host):
-    assert host.podman("route-planner-deployment-pod-fast-api").is_running
+    assert host.podman("wekan-board").is_running
+
+
+def test_mongo_pod_is_running(host):
+    assert host.podman("wekan-db").is_running
 
 
 def test_health_route(host):
     cmd = host.run("curl -I localhost:8080/login")
-    assert "alive" in cmd.stdout
+    assert "200" in cmd.stdout
 
 
 def test_port_is_opened(host):
     assert host.socket("tcp://0.0.0.0:8080").is_listening
 
-
-def test_mongodb_pod_is_running(host):
-    assert host.podman("route-planner-deployment-pod-mongodb").is_running

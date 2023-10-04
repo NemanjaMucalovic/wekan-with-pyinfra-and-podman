68b8cce3fcd4d7a27d7ce402a1a628b1a445335e
Nemanja Mucalovic <nemanjamucalovic@gmail.com>
2023-10-04 15:10:18 +0200

    created directories and files on host, tests are passing

--- deployment.py
+++ deployment.py
@@ -1,8 +1,32 @@
-from pyinfra.operations import dnf
+from pyinfra.operations import dnf, server, files
 
 dnf.packages(
     name="install podman",
     packages=["podman"],
     update=True,
     _sudo=True,
+)
+
+server.shell(name="pull container images", commands=["podman pull docker.io/library/mongo; podman pull docker.io/wekanteam/wekan"])
+
+files.directory(
+    name="create project directory",
+    path="~/wekan/",
+)
+
+files.template(
+    name="Create kube file",
+    src="templates/kube.j2",
+    dest="~/wekan/kube.yml"
+)
+
+files.directory(
+    name="create dir for quadlet files",
+    path="~/.config/containers/systemd",
+)
+
+files.template(
+    name="Create podman service file",
+    src="templates/wekan.j2",
+    dest="~/.config/containers/systemd/wekan.kube",
 )
--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -2,6 +2,27 @@ def test_if_podman_is_installed(host):
     assert host.package("podman").is_installed
 
 
+def test_if_images_are_pulled(host):
+    cmd = host.check_output("podman images")
+    assert "docker.io/library/mongo" and "docker.io/wekanteam/wekan" in cmd
+
+
+def test_if_wekan_dir_is_there(host):
+    assert host.file("~/wekan").is_directory
+
+
+def test_if_kube_yaml_is_copied(host):
+    assert host.file("~/wekan/kube.yml").exists
+
+
+def test_if_systemd_dir_is_there(host):
+    assert host.file("~/.config/containers/systemd/").is_directory
+
+
+def test_if_quadlet_yaml_is_copied(host):
+    assert host.file("~/.config/containers/systemd/wekan.kube").exists
+
+
 def test_backend_pod_is_running(host):
     assert host.podman("route-planner-deployment-pod-fast-api").is_running
 
